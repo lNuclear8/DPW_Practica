cf313002de6cc06c4f0e5ebe062259cbb0f09f1c
lNuclear8 <151663815+lNuclear8@users.noreply.github.com>
2024-12-15 20:55:41 +0100

    feat: server.js

--- server.js
+++ server.js
@@ -5,12 +5,17 @@ const app = express();
 // Puerto donde se ejecutará el servidor
 const PORT = 3000;
 
-// Ruta para servir archivos estáticos
+// Ruta para servir archivos estáticos desde el directorio actual
 app.use(express.static(path.join(__dirname)));
 
 // Ruta principal para redirigir al archivo index.html
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+// Manejo de rutas no encontradas (opcional)
+app.use((req, res) => {
+  res.status(404).send('Página no encontrada');
 });
 
 // Iniciar el servidor
